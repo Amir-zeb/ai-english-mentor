936423c01f5091a6435bce8bc295d1a1d7d61632
Amir-zeb <amirzeb695@gmail.com>
2026-07-08 17:59:18 +0500

    create model for conversation history

---
+++ lib/models/ConversationHistory.ts
@@ -0,0 +1,20 @@
+import mongoose, { Schema, models, model } from "mongoose";
+
+export interface IConversationHistory {
+    _id: mongoose.Types.ObjectId;
+    title: string;
+    model: string;
+    createdAt: Date;
+    updatedAt: Date;
+}
+
+const ConversationHistorySchema = new Schema<IConversationHistory>(
+    {
+        title: { type: String, default: "New conversation" },
+        model: { type: String, required: true },
+    },
+    { timestamps: true }
+);
+
+export const ConversationHistory =
+    models.ConversationHistory || model<IConversationHistory>("ConversationHistory", ConversationHistorySchema);
