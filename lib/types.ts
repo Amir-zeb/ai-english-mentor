4a227a3f02c19d1435cf24497fef33680e1f6cb6
Amir-zeb <amirzeb695@gmail.com>
2026-07-08 22:07:13 +0500

    declare new types

--- lib/types.ts
+++ lib/types.ts
@@ -2,15 +2,38 @@ import { ROLES } from "./constant";
 
 export type ChatRole = typeof ROLES[keyof typeof ROLES];
 
-export type ChatMessage = {
+export type ChatResponseBody = {
+    reply: string;
+};
+
+export type ConversationSummaryT = {
+    _id: string;
+    title: string;
+    updatedAt: string;
+};
+
+export type ChatSideBarProps = {
+    title?: string;
+    history?: ConversationSummaryT[];
+    activeConversationId?: string | null;
+    onSelectConversation?: (id: string) => void;
+    onNewConversation?: () => void;
+};
+
+export type ConversationMessageT = {
     role: ChatRole;
     content: string;
+    createdAt?: string;
+};
+
+export type ConversationDetailT = {
+    conversation: {
+        _id: string;
+        title: string;
+    };
+    messages: ConversationMessageT[];
 };
 
 export type ChatRequestBody = {
-    messages: ChatMessage[];
-};
-
-export type ChatResponseBody = {
-    reply: string;
+    messages: ConversationMessageT[];
 };
