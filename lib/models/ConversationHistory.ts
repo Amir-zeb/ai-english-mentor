58786b4c2c42a41c5ae6fdebc5c3443977b9265e
Amir-zeb <amirzeb695@gmail.com>
2026-07-11 21:51:55 +0500

    conversationHistory - model - app mentorName key

--- lib/models/ConversationHistory.ts
+++ lib/models/ConversationHistory.ts
@@ -4,6 +4,7 @@ export interface IConversationHistory {
     _id: mongoose.Types.ObjectId;
     title: string;
     model: string;
+    mentorName: string;
     userId: mongoose.Types.ObjectId;
     createdAt: Date;
     updatedAt: Date;
@@ -14,6 +15,7 @@ const ConversationHistorySchema = new Schema<IConversationHistory>(
         title: { type: String, default: "New conversation", trim: true, required: true },
         model: { type: String, required: true },
         userId: { type: Schema.Types.ObjectId, ref: "User", required: true },
+        mentorName: { type: String, required: true }
     },
     { timestamps: true }
 );
